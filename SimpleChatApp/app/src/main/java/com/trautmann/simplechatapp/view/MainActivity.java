660e9b162c91b8a1d59d5a74fdca82f01d5ef9c6
Brandon Trautmann <brantrautmann@gmail.com>
2017-07-27 18:30:29 -0400

    Deleted the getChats method because I tested chats list call and it worked, so now will go forward with actual implementation.

--- SimpleChatApp/app/src/main/java/com/trautmann/simplechatapp/view/MainActivity.java
+++ SimpleChatApp/app/src/main/java/com/trautmann/simplechatapp/view/MainActivity.java
@@ -18,10 +18,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
 
-        getChats();
     }
-
-    private void getChats() {
-
-    }
+    
 }
