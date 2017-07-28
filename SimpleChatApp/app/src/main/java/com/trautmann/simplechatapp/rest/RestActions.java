45b34d982f98866b35feb9270a52eee556110e2e
Brandon Trautmann <brantrautmann@gmail.com>
2017-07-28 15:11:45 -0400

    Add CreateChatMessage call in RestActions and service class

--- SimpleChatApp/app/src/main/java/com/trautmann/simplechatapp/rest/RestActions.java
+++ SimpleChatApp/app/src/main/java/com/trautmann/simplechatapp/rest/RestActions.java
@@ -1,5 +1,6 @@
 package com.trautmann.simplechatapp.rest;
 
+import com.trautmann.simplechatapp.rest.response.CreateChatMessage;
 import com.trautmann.simplechatapp.rest.response.GetChatMessagesList;
 import com.trautmann.simplechatapp.rest.response.GetChatsList;
 import com.trautmann.simplechatapp.rest.service.ChatsService;
@@ -21,6 +22,12 @@ public class RestActions {
     public static Single<GetChatMessagesList> getChatMessagesList(int chatId) {
         RestAction<GetChatMessagesList> action = new RestAction<>(
                 ServiceCreator.createService(ChatsService.class).getChatMessagesList(chatId));
+        return action.perform();
+    }
+
+    public static Single<CreateChatMessage> createChatMessage(int chatId, String message) {
+        RestAction<CreateChatMessage> action = new RestAction<>(
+                ServiceCreator.createService(ChatsService.class).createChatMessage(chatId, message));
         return action.perform();
     }
 }
--- SimpleChatApp/app/src/main/java/com/trautmann/simplechatapp/rest/service/ChatsService.java
+++ SimpleChatApp/app/src/main/java/com/trautmann/simplechatapp/rest/service/ChatsService.java
@@ -1,12 +1,15 @@
 package com.trautmann.simplechatapp.rest.service;
 
+import com.trautmann.simplechatapp.rest.response.CreateChatMessage;
 import com.trautmann.simplechatapp.rest.response.GetChatMessagesList;
 import com.trautmann.simplechatapp.rest.response.GetChatsList;
 
 import io.reactivex.Single;
 import retrofit2.Response;
 import retrofit2.http.GET;
+import retrofit2.http.POST;
 import retrofit2.http.Path;
+import retrofit2.http.Query;
 
 /**
  * Created by Brandon Trautmann
@@ -19,5 +22,9 @@ public interface ChatsService {
 
     @GET("chats/{id}/chat_messages?page=1&limit=50")
     Single<Response<GetChatMessagesList>> getChatMessagesList(@Path(value = "id") int chatId);
+
+    @POST("chats/{id}/chat_messages")
+    Single<Response<CreateChatMessage>> createChatMessage(@Path(value = "id") int chatId,
+                                                          @Query("message") String message);
 
 }
