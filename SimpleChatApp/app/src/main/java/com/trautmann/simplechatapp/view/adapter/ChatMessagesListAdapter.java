8514bb0ef44014ce3d23f04f3d91a31bbd5fc3e1
Brandon Trautmann <brantrautmann@gmail.com>
2017-07-28 06:39:17 -0400

    Add chat message list item layout as well as the corresponding view model and adapter

---
+++ SimpleChatApp/app/src/main/java/com/trautmann/simplechatapp/view/adapter/ChatMessagesListAdapter.java
@@ -0,0 +1,67 @@
+package com.trautmann.simplechatapp.view.adapter;
+
+import android.content.Context;
+import android.databinding.DataBindingUtil;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+
+import com.trautmann.simplechatapp.R;
+import com.trautmann.simplechatapp.databinding.ChatMessageListItemBinding;
+import com.trautmann.simplechatapp.rest.model.ChatMessage;
+import com.trautmann.simplechatapp.viewmodel.ChatMessageListItemViewModel;
+
+import java.util.List;
+
+/**
+ * Created by Brandon Trautmann
+ */
+
+public class ChatMessagesListAdapter extends RecyclerView.Adapter<ChatMessagesListAdapter.ChatMessageBindingHolder> {
+
+    private List<ChatMessage> chatMessages;
+    private Context context;
+
+    public ChatMessagesListAdapter(List<ChatMessage> chatMessages, Context context) {
+        this.chatMessages = chatMessages;
+        this.context = context;
+    }
+
+    public List<ChatMessage> getChats() {
+        return chatMessages;
+    }
+
+    public void setChats(List<ChatMessage> chats) {
+        this.chatMessages = chats;
+    }
+
+    @Override
+    public ChatMessageBindingHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        ChatMessageListItemBinding chatMessageListItemBinding = DataBindingUtil.inflate(
+                LayoutInflater.from(parent.getContext()),
+                R.layout.chat_list_item,
+                parent,
+                false);
+        return new ChatMessageBindingHolder(chatMessageListItemBinding);
+    }
+
+    @Override
+    public void onBindViewHolder(ChatMessageBindingHolder holder, int position) {
+        ChatMessageListItemBinding chatMessageListItemBinding = holder.chatMessageListItemBinding;
+        chatMessageListItemBinding.setViewModel(new ChatMessageListItemViewModel(context, chatMessages.get(position)));
+    }
+
+    @Override
+    public int getItemCount() {
+        return chatMessages == null ? 0 : chatMessages.size();
+    }
+
+    public static class ChatMessageBindingHolder extends RecyclerView.ViewHolder {
+        private ChatMessageListItemBinding chatMessageListItemBinding;
+
+        public ChatMessageBindingHolder(ChatMessageListItemBinding chatMessageListItemBinding) {
+            super(chatMessageListItemBinding.chatMessageContainer);
+            this.chatMessageListItemBinding = chatMessageListItemBinding;
+        }
+    }
+}
---
+++ SimpleChatApp/app/src/main/java/com/trautmann/simplechatapp/viewmodel/ChatMessageListItemViewModel.java
@@ -0,0 +1,39 @@
+package com.trautmann.simplechatapp.viewmodel;
+
+import android.content.Context;
+
+import com.trautmann.simplechatapp.rest.model.ChatMessage;
+
+/**
+ * Created by Brandon Trautmann
+ */
+
+public class ChatMessageListItemViewModel {
+
+
+    private Context context;
+    private ChatMessage chatMessage;
+
+    public ChatMessageListItemViewModel(Context context, ChatMessage chatMessage) {
+        this.context = context;
+        this.chatMessage = chatMessage;
+    }
+
+    public ChatMessage getChatMessage() {
+        return chatMessage;
+    }
+
+    public void setChatMessage(ChatMessage chatMessage) {
+        this.chatMessage = chatMessage;
+    }
+
+    public String getSenderName() {
+        return chatMessage.getSender().getName();
+    }
+
+    public String getMessageBody() {
+        return chatMessage.getMessage();
+    }
+
+
+}
