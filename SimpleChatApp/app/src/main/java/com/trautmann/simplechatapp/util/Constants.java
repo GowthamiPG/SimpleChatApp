bf0d7ed8c583ef77c5f391a4e922108429449ba1
Brandon Trautmann <brantrautmann@gmail.com>
2017-07-28 06:13:14 -0400

    Adding Intent Argument constants for chat name and id

---
+++ SimpleChatApp/app/src/main/java/com/trautmann/simplechatapp/util/Constants.java
@@ -0,0 +1,13 @@
+package com.trautmann.simplechatapp.util;
+
+/**
+ * Created by Brandon Trautmann
+ */
+
+public class Constants {
+
+    public static class IntentArguments {
+        public static final String CHAT_ID = "chatId";
+        public static final String CHAT_NAME = "chatName";
+    }
+}
