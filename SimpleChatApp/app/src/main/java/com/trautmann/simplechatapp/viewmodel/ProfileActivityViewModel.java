a3910c4616238695d44bd4e3471cb11b392abd3b
Brandon Trautmann <brantrautmann@gmail.com>
2017-07-28 16:27:00 -0400

    Add binding methods and getters and setters for profile view model class

--- SimpleChatApp/app/src/main/java/com/trautmann/simplechatapp/viewmodel/ProfileActivityViewModel.java
+++ SimpleChatApp/app/src/main/java/com/trautmann/simplechatapp/viewmodel/ProfileActivityViewModel.java
@@ -1,8 +1,43 @@
 package com.trautmann.simplechatapp.viewmodel;
 
+import android.content.Context;
+import android.databinding.BaseObservable;
+
+import com.trautmann.simplechatapp.rest.model.User;
+
 /**
  * Created by Brandon Trautmann
  */
 
-public class ProfileActivityViewModel {
+public class ProfileActivityViewModel extends BaseObservable {
+
+    private Context context;
+    private User user;
+
+    public ProfileActivityViewModel(Context context, User user) {
+        this.context = context;
+        this.user = user;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public String getUserName() {
+        return getUser().getName();
+    }
+
+    public String getUserEmail() {
+        return getUser().getEmail();
+    }
+
+    public void getProfile() {
+
+    }
+
+
 }
