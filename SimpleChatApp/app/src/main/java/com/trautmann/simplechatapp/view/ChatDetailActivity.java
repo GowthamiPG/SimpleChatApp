6d36fcdf8d0b43e1a577fdd699f8294b892e14e0
Brandon Trautmann <brantrautmann@gmail.com>
2017-07-28 15:12:50 -0400

    Instead of databinding the onclick for send message button, use a standard onclick listener in the UI class. i can't figure out how to update the recycler view from within the view model, or pass that back response back to the UI without first going through the UI, so we can figure that out later.

--- SimpleChatApp/app/src/main/java/com/trautmann/simplechatapp/view/ChatDetailActivity.java
+++ SimpleChatApp/app/src/main/java/com/trautmann/simplechatapp/view/ChatDetailActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
+import android.text.TextUtils;
 import android.util.Log;
 import android.widget.Toast;
 
@@ -36,20 +37,40 @@ public class ChatDetailActivity extends AppCompatActivity {
         String chatName = getIntent().getExtras().getString(Constants.IntentArguments.CHAT_NAME);
         binding.setViewModel(new ChatDetailActivityViewModel(this,
                 new Chat(chatId, chatName, null, null)));
-        binding.setNewMessageInput(binding.chatDetailInputEditText);
 
         setSupportActionBar(binding.chatDetailToolbar);
         if (getSupportActionBar() != null) {
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         }
 
-        LinearLayoutManager lm = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, true);
+        LinearLayoutManager lm = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
         binding.chatMessagesListRecyclerView.addItemDecoration(new ChatMessageItemDecoration(this));
         binding.chatMessagesListRecyclerView.setLayoutManager(lm);
         adapter = new ChatMessagesListAdapter(null, this);
         binding.chatMessagesListRecyclerView.setAdapter(adapter);
 
+        handleSendButtonClicks();
+
         getChatMessages(chatId);
+
+    }
+
+    //TODO: Use Data binding to handle onClick
+    // Can't figure out how to update RecyclerView using binding
+    private void handleSendButtonClicks() {
+        binding.chatDetailSendImageButton.setOnClickListener(view -> {
+            if (!TextUtils.isEmpty(binding.chatDetailInputEditText.getEditableText())) {
+                binding.getViewModel().createChatMessage(
+                        binding.chatDetailInputEditText.getEditableText().toString())
+                        .subscribe(createChatMessage -> {
+                            adapter.getChatMessages().add(createChatMessage.getMessageSent());
+                            adapter.notifyDataSetChanged();
+                        }, throwable -> {
+                            Toast.makeText(this, "Error sending message", Toast.LENGTH_SHORT).show();
+                        });
+
+            }
+        });
     }
 
     private void getChatMessages(int chatId) {
--- SimpleChatApp/app/src/main/java/com/trautmann/simplechatapp/viewmodel/ChatDetailActivityViewModel.java
+++ SimpleChatApp/app/src/main/java/com/trautmann/simplechatapp/viewmodel/ChatDetailActivityViewModel.java
@@ -1,12 +1,16 @@
 package com.trautmann.simplechatapp.viewmodel;
 
 import android.content.Context;
-import android.util.Log;
+import android.databinding.BaseObservable;
+import android.databinding.ObservableArrayList;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.EditText;
 
 import com.trautmann.simplechatapp.rest.RestActions;
 import com.trautmann.simplechatapp.rest.model.Chat;
+import com.trautmann.simplechatapp.rest.model.ChatMessage;
+import com.trautmann.simplechatapp.rest.response.CreateChatMessage;
 import com.trautmann.simplechatapp.rest.response.GetChatMessagesList;
 
 import io.reactivex.Single;
@@ -15,7 +19,7 @@ import io.reactivex.Single;
  * Created by Brandon Trautmann
  */
 
-public class ChatDetailActivityViewModel {
+public class ChatDetailActivityViewModel extends BaseObservable{
 
     private Context context;
     private Chat chat;
@@ -37,15 +41,30 @@ public class ChatDetailActivityViewModel {
         return chat.getName();
     }
 
+    public int getChatId() {
+        return chat.getId();
+    }
+
+    public final ObservableArrayList<ChatMessage> chatMessages =
+            new ObservableArrayList<>();
+
+    public ObservableArrayList<ChatMessage> getChatMessages() {
+        return chatMessages;
+    }
+
     public Single<GetChatMessagesList> getChatMessagesList(int chatId) {
         return RestActions.getChatMessagesList(chatId);
     }
 
-    public View.OnClickListener sendMessage(EditText textInput) {
-        return new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Log.d("LOG", "Send: " + textInput.getEditableText().toString());
+    public Single<CreateChatMessage> createChatMessage(String message) {
+        return RestActions.createChatMessage(getChatId(), message);
+    }
+
+    // When we go back to data binding onClick, use this
+    public View.OnClickListener sendMessage(int chatId, EditText textInput) {
+        return view -> {
+            if (!TextUtils.isEmpty(textInput.getEditableText().toString())) {
+                createChatMessage(textInput.getEditableText().toString());
             }
         };
     }
