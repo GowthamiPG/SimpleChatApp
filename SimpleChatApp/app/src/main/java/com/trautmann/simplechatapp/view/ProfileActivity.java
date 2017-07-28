e66d7af637967dc37a9a1652c8bdd479a96113bb
Brandon Trautmann <brantrautmann@gmail.com>
2017-07-28 16:21:13 -0400

    Add ProfileActivity along with layout file and view model

---
+++ SimpleChatApp/app/src/main/java/com/trautmann/simplechatapp/view/ProfileActivity.java
@@ -0,0 +1,10 @@
+package com.trautmann.simplechatapp.view;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Brandon Trautmann
+ */
+
+public class ProfileActivity extends AppCompatActivity {
+}
---
+++ SimpleChatApp/app/src/main/java/com/trautmann/simplechatapp/viewmodel/ProfileActivityViewModel.java
@@ -0,0 +1,8 @@
+package com.trautmann.simplechatapp.viewmodel;
+
+/**
+ * Created by Brandon Trautmann
+ */
+
+public class ProfileActivityViewModel {
+}
