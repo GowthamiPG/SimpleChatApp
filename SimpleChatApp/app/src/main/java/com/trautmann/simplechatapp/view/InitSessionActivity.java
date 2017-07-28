4c22eef8891544078911452c814d873e9b787523
Brandon Trautmann <brantrautmann@gmail.com>
2017-07-28 18:49:34 -0400

    Rename username edit text to email to be more accurate, and use binding method for handling login click instead of click listener in the Activity. onClick calls directly through to click listener in view model.

--- SimpleChatApp/app/src/main/java/com/trautmann/simplechatapp/view/InitSessionActivity.java
+++ SimpleChatApp/app/src/main/java/com/trautmann/simplechatapp/view/InitSessionActivity.java
@@ -1,11 +1,9 @@
 package com.trautmann.simplechatapp.view;
 
-import android.content.Intent;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
 
 import com.trautmann.simplechatapp.R;
 import com.trautmann.simplechatapp.databinding.InitSessionActivityBinding;
@@ -30,32 +28,7 @@ public class InitSessionActivity extends AppCompatActivity {
             binding =
                     DataBindingUtil.setContentView(this, R.layout.init_session_activity);
             binding.setViewModel(new InitSessionViewModel(this));
-
-            handleLoginClicks();
         }
-    }
-
-    private void handleLoginClicks() {
-        binding.loginButton.setOnClickListener(view -> {
-            if (binding.getViewModel().areValidLoginInputs(
-                    binding.usernameEditText, binding.passwordEditText)) {
-                String email = binding.usernameEditText.getEditableText().toString();
-                String password = binding.passwordEditText.getEditableText().toString();
-                binding.getViewModel().logUserIn(email, password)
-                        .doOnSubscribe(disposable -> {
-                            binding.loginButton.setVisibility(View.GONE);
-                            binding.loginProgressBar.setVisibility(View.VISIBLE);
-                        })
-                        .subscribe(genericResponse -> {
-                            Intent intent = new Intent(InitSessionActivity.this, MainActivity.class);
-                            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
-                            startActivity(intent);
-                        }, throwable -> {
-                            binding.loginProgressBar.setVisibility(View.GONE);
-                            binding.loginButton.setVisibility(View.VISIBLE);
-                        });
-            }
-        });
     }
 
     private boolean userAlreadyLoggedIn() {
--- SimpleChatApp/app/src/main/java/com/trautmann/simplechatapp/viewmodel/InitSessionViewModel.java
+++ SimpleChatApp/app/src/main/java/com/trautmann/simplechatapp/viewmodel/InitSessionViewModel.java
@@ -1,24 +1,47 @@
 package com.trautmann.simplechatapp.viewmodel;
 
 import android.content.Context;
+import android.content.Intent;
+import android.databinding.BaseObservable;
 import android.text.TextUtils;
+import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.android.databinding.library.baseAdapters.BR;
 import com.trautmann.simplechatapp.rest.RestActions;
-import com.trautmann.simplechatapp.rest.response.GenericResponse;
-
-import io.reactivex.Single;
+import com.trautmann.simplechatapp.view.MainActivity;
 
 /**
  * Created by Brandon Trautmann
  */
 
-public class InitSessionViewModel {
+public class InitSessionViewModel extends BaseObservable{
 
     private Context context;
+    private boolean isLoggingIn;
 
     public InitSessionViewModel(Context context) {
         this.context = context;
+        setLoggingIn(false);
+    }
+
+    public boolean isLoggingIn() {
+        return isLoggingIn;
+    }
+
+    public void setLoggingIn(boolean loggingIn) {
+        isLoggingIn = loggingIn;
+    }
+
+    public View.OnClickListener onLoginClicked(EditText emailInput, EditText passwordInput) {
+        return view -> {
+            if (areValidLoginInputs(emailInput, passwordInput)) {
+                String email = emailInput.getEditableText().toString();
+                String password = emailInput.getEditableText().toString();
+                logUserIn(email, password);
+            }
+        };
     }
 
     public boolean areValidLoginInputs(EditText loginInput, EditText passwordInput) {
@@ -26,8 +49,27 @@ public class InitSessionViewModel {
                 && !TextUtils.isEmpty(passwordInput.getEditableText().toString());
     }
 
-    public Single<GenericResponse> logUserIn(String login, String password) {
-        return RestActions.login(login, password);
+    public void logUserIn(String email, String password) {
+        RestActions.login(email, password)
+                .doOnSubscribe(disposable -> {
+                    setLoggingIn(true);
+                    notifyPropertyChanged(BR._all);
+                })
+                .subscribe(genericResponse -> {
+                    setLoggingIn(false);
+                    notifyPropertyChanged(BR._all);
+                    launchMainActivity();
+                }, throwable -> {
+                    setLoggingIn(false);
+                    notifyPropertyChanged(BR._all);
+                    Toast.makeText(context, "Login failed", Toast.LENGTH_SHORT).show();
+                });
+    }
+
+    private void launchMainActivity() {
+        Intent intent = new Intent(context, MainActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+        context.startActivity(intent);
     }
 
     public void registerUser() {
