7ecb87acb9dfa2187e124d15dff4f871d43cd710
Brandon Trautmann <brantrautmann@gmail.com>
2017-07-28 16:39:02 -0400

    Implement onCreate in profile activity. Bind the views and call getProfile()

--- SimpleChatApp/app/src/main/java/com/trautmann/simplechatapp/view/ProfileActivity.java
+++ SimpleChatApp/app/src/main/java/com/trautmann/simplechatapp/view/ProfileActivity.java
@@ -1,10 +1,30 @@
 package com.trautmann.simplechatapp.view;
 
+import android.databinding.DataBindingUtil;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+
+import com.trautmann.simplechatapp.R;
+import com.trautmann.simplechatapp.databinding.ProfileActivityBinding;
+import com.trautmann.simplechatapp.viewmodel.ProfileActivityViewModel;
 
 /**
  * Created by Brandon Trautmann
  */
 
 public class ProfileActivity extends AppCompatActivity {
+
+
+    ProfileActivityBinding binding;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = DataBindingUtil.setContentView(this, R.layout.profile_activity);
+        binding.setViewModel(new ProfileActivityViewModel(this));
+
+        binding.getViewModel().getProfile();
+    }
 }
