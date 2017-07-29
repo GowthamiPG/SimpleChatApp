5c117985d39f40a6949b5323c260b80b3cf7c1b8
Brandon Trautmann <brantrautmann@gmail.com>
2017-07-29 12:47:22 -0400

    CreateUser response class created and call added to UserService

--- SimpleChatApp/app/src/main/java/com/trautmann/simplechatapp/rest/service/UserService.java
+++ SimpleChatApp/app/src/main/java/com/trautmann/simplechatapp/rest/service/UserService.java
@@ -1,10 +1,12 @@
 package com.trautmann.simplechatapp.rest.service;
 
+import com.trautmann.simplechatapp.rest.response.CreateUser;
 import com.trautmann.simplechatapp.rest.response.GetCurrentUser;
 
 import io.reactivex.Single;
 import retrofit2.Response;
 import retrofit2.http.GET;
+import retrofit2.http.POST;
 
 /**
  * Created by Brandon Trautmann
@@ -14,5 +16,9 @@ public interface UserService {
 
     @GET("users/current")
     Single<Response<GetCurrentUser>> getCurrentUser();
+
+    @POST("users")
+    Single<Response<CreateUser>> createUser();
+
 
 }
