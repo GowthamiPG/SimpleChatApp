36f1c4fe25fd7056fe39422b8a551b6ef2791030
Brandon Trautmann <brantrautmann@gmail.com>
2017-07-29 12:41:43 -0400

    Re-type update chat to a PATCH instead of POST

--- SimpleChatApp/app/src/main/java/com/trautmann/simplechatapp/rest/service/ChatsService.java
+++ SimpleChatApp/app/src/main/java/com/trautmann/simplechatapp/rest/service/ChatsService.java
@@ -11,6 +11,7 @@ import retrofit2.Response;
 import retrofit2.http.Field;
 import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
+import retrofit2.http.PATCH;
 import retrofit2.http.POST;
 import retrofit2.http.Path;
 import retrofit2.http.Query;
@@ -36,7 +37,7 @@ public interface ChatsService {
     Single<Response<CreateChat>> createChat(@Field("name") String chatName,
                                             @Field("message") String firstChatMessage);
 
-    @POST("chats/{id}")
+    @PATCH("chats/{id}")
     @FormUrlEncoded
     Single<Response<UpdateChat>> updateChat(@Path(value = "id") int chatId,
                                             @Field("name") String newChatName);
