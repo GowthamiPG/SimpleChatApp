9bdbb0671b920eef7c4f42ca898a5b14c6f730f2
Brandon Trautmann <brantrautmann@gmail.com>
2017-07-30 18:40:42 -0400

    Adding initial state test for main activity

--- SimpleChatApp/app/src/androidTest/java/com/trautmann/simplechatapp/MainActivityTest.java
+++ SimpleChatApp/app/src/androidTest/java/com/trautmann/simplechatapp/MainActivityTest.java
@@ -1,10 +1,38 @@
 package com.trautmann.simplechatapp;
 
+import android.support.test.espresso.matcher.ViewMatchers;
+import android.support.test.rule.ActivityTestRule;
+
+import com.trautmann.simplechatapp.view.MainActivity;
+
+import org.junit.Rule;
+import org.junit.Test;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withEffectiveVisibility;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+
 /**
  * Created by Brandon Trautmann
  */
 
 public class MainActivityTest {
 
-    
+    @Rule
+    public ActivityTestRule<MainActivity> activityTestRule =
+            new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void initialState_test() {
+        onView(withId(R.id.createChatFab))
+                .check(matches(withEffectiveVisibility(ViewMatchers.Visibility.VISIBLE)));
+
+        onView(withId(R.id.chatListRecyclerView))
+                .check(matches(withEffectiveVisibility(ViewMatchers.Visibility.VISIBLE)));
+
+        onView(withId(R.id.profileButton))
+                .check(matches(withEffectiveVisibility(ViewMatchers.Visibility.VISIBLE)));
+
+    }
 }
