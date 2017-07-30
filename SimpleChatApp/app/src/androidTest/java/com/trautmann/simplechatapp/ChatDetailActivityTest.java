4dbe1a843bd26e4853d34df8ae793c97f473d6e0
Brandon Trautmann <brantrautmann@gmail.com>
2017-07-30 19:04:21 -0400

    Added instrumented tests for rename chat dialog. Tests title, edit texts and pos and neg buttons.

--- SimpleChatApp/app/src/androidTest/java/com/trautmann/simplechatapp/ChatDetailActivityTest.java
+++ SimpleChatApp/app/src/androidTest/java/com/trautmann/simplechatapp/ChatDetailActivityTest.java
@@ -13,9 +13,12 @@ import org.junit.Rule;
 import org.junit.Test;
 
 import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.withEffectiveVisibility;
+import static android.support.test.espresso.matcher.ViewMatchers.withHint;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
 /**
  * Created by Brandon Trautmann
@@ -52,6 +55,30 @@ public class ChatDetailActivityTest {
         onView(withId(R.id.editChatButton))
                 .check(matches(withEffectiveVisibility(ViewMatchers.Visibility.VISIBLE)));
 
+    }
+
+    @Test
+    public void editChatName_test() {
+        onView(withId(R.id.editChatButton))
+                .check(matches(withEffectiveVisibility(ViewMatchers.Visibility.VISIBLE)));
+
+        onView(withId(R.id.editChatButton))
+                .perform(click());
+
+        onView(withText(R.string.rename_chat_dialog_title))
+                .check(matches(withEffectiveVisibility(ViewMatchers.Visibility.VISIBLE)));
+
+        onView(withId(R.id.chatNameEditText))
+                .check(matches(withEffectiveVisibility(ViewMatchers.Visibility.VISIBLE)));
+
+        onView(withId(R.id.chatNameEditText))
+                .check(matches(withHint(R.string.new_chat_name_hint_text)));
+
+        onView(withText(R.string.rename_chat_dialog_rename_button_text))
+                .check(matches(withEffectiveVisibility(ViewMatchers.Visibility.VISIBLE)));
+
+        onView(withText(R.string.new_chat_dialog_cancel_button_text))
+                .check(matches(withEffectiveVisibility(ViewMatchers.Visibility.VISIBLE)));
     }
 
 }
